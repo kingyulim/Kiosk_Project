ff176cb8c9410dc95340114d464eaed3abb96a84
Kingyul Kim <youremail@example.com>
2025-10-27 16:34:46 +0900

    level5 완료 202510271634

---
+++ src/Level5/Main.java
@@ -0,0 +1,9 @@
+package Level5;
+
+public class Main {
+    public static void main(String[] args) {
+        Kiosk Kiosk = new Kiosk();
+
+        Kiosk.start();
+    }
+}
---
+++ src/Level5/Menu.java
@@ -0,0 +1,39 @@
+package Level5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    private final String menuCategoryName;
+    private final List<MenuItem> menuItems;
+
+    Menu(String data1) {
+        this.menuCategoryName = data1;
+
+        this.menuItems = new ArrayList<>();
+    }
+
+    /**
+     * 캡술화된 getMenuCategoryName 가져오는 객체
+     * @return
+     */
+    String getMenuCategoryName() {
+        return menuCategoryName;
+    }
+
+    /**
+     * 캡슐화된 menuItems에 저장된 배열을 가져오는 객체
+     * @return
+     */
+    List<MenuItem> getMenuItem() {
+        return this.menuItems;
+    }
+
+    /**
+     * 캡슐화된 addMenuItem 에 배열을 넣는 함수
+     * @param data1
+     */
+    void addMenuItem(MenuItem data1) {
+        this.menuItems.add(data1);
+    }
+}
